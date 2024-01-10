49674053dbeb3a210afd2cf6e8d0280623b31456
aahan.agrawal <aahan.agrawal@veeva.com>
2024-01-10 19:51:48 +0000

    Add prod tag

--- main.py
+++ main.py
@@ -114,7 +114,7 @@ def go(config: DictConfig):
                 os.path.join(hydra.utils.get_original_cwd(), "components", "test_regression_model"),
                 "main",
                 parameters={
-                    "mlflow_model": "model_export:v12",
+                    "mlflow_model": "model_export:prod",
                     "test_dataset": "test_data.csv:latest"
                 },
             )
